1411c7d9c9bece1855b6b3d58ebb6f21402b6a1b
PassivePicasso <PassivePicasso@users.noreply.github.com>
2021-12-18 23:35:24 -0500

    reconfigure CreateResourceBundles to start creating multiple outputs

--- Editor/PipelineJobs/CreateResourceBundle.cs
+++ Editor/PipelineJobs/CreateResourceBundle.cs
@@ -1,14 +1,13 @@
 ﻿using AssetsTools.NET;
 using AssetsTools.NET.Extra;
-using BundleKit.Assets;
 using BundleKit.Utility;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using ThunderKit.Common.Logging;
+using ThunderKit.Core.Data;
 using ThunderKit.Core.Pipelines;
 using UnityEditor;
 
@@ -20,7 +19,7 @@ namespace BundleKit.PipelineJobs
     public class CreateResourceBundle : PipelineJob
     {
         public DefaultAsset bundle;
-        public string outputAssetBundlePath;
+        public string outputDirectory;
         public string[] nameRegexFilters;
         public AssetClassID[] classes;
 
@@ -29,78 +28,101 @@ namespace BundleKit.PipelineJobs
         {
             using (var progressBar = new ProgressBar("Constructing AssetBundle"))
             {
+                if (!AssetDatabase.IsValidFolder(outputDirectory))
+                    return Task.CompletedTask;
+
+                var settings = ThunderKitSetting.GetOrCreateSettings<ThunderKitSettings>();
+                var dataDirectoryPath = Path.Combine(settings.GamePath, $"{Path.GetFileNameWithoutExtension(settings.GameExecutable)}_Data");
+                var sharedAssetsFiles = Directory.EnumerateFiles(dataDirectoryPath, "sharedassets*.assets").ToArray();
+                var sharedAssetsresSFiles = Directory.EnumerateFiles(dataDirectoryPath, "sharedassets*.assets.resS").ToArray();
+                var resourcesFilePath = Path.Combine(dataDirectoryPath, "resources.assets");
+                var ggmPath = Path.Combine(dataDirectoryPath, "globalgamemanagers");
+                var path = AssetDatabase.GetAssetPath(bundle);
+
                 InitializeContainers(nameRegexFilters, out var assetsReplacers, out var contexts, out var newContainerChildren, out var nameRegex);
-                InitializePaths(bundle, outputAssetBundlePath, out var resourcesFilePath, out var ggmPath, out var fileName, out var path, out var dataDirectoryPath);
 
                 am = new AssetsManager();
                 var resourcesInst = am.InitializeAssetTools(resourcesFilePath);
 
-                var assets = CollectAssets(am, resourcesInst, nameRegex, classes, progressBar)
-                                    .Where(asset => asset.AssetFileName.Equals("resources.assets"))
+                var allAssets = CollectAssets(am, resourcesInst, nameRegex, classes, progressBar)
+                                    //.Where(asset => asset.AssetFileName.Equals("resources.assets"))
                                     .Distinct()
                                     .OrderBy(ta => ta.PathId)
                                     .ToArray();
 
-                am.PrepareNewBundle(path, out var bun, out var bundleAssetsFile, out var assetBundleExtAsset);
-
-                // Update bundle assets name and bundle name to the name specified in outputAssetBundlePath
-                var bundleBaseField = assetBundleExtAsset.instance.GetBaseField();
-                bundleBaseField.SetValue("m_Name", fileName);
-                bundleBaseField.SetValue("m_AssetBundleName", fileName);
-
-                bundleAssetsFile.file.dependencies.dependencies.Clear();
-                foreach (var dependency in resourcesInst.file.dependencies.dependencies)
-                    bundleAssetsFile.AddDependency(dependency.assetPath);
-
-                UpdateAssetBundleDependencies(bundleBaseField, bundleAssetsFile.file.dependencies.dependencies);
-
-                // Get container for populating asset listings
-                var containerArray = bundleBaseField.GetField("m_Container/Array");
-                foreach (var (asset, assetName, assetFileName, fileId, pathId, depth) in assets)
+                var assetsByFile = allAssets.GroupBy(data => data.AssetFileName);
+                foreach (var assetGroup in assetsByFile)
                 {
-                    var assetBaseField = asset.instance.GetBaseField();
-                    if (assetBaseField.GetFieldType() == "Texture2D")
+                    newContainerChildren.Clear();
+                    assetsReplacers.Clear();
+                    var assets = assetGroup.Distinct().ToList();
+                    string fileName = assetGroup.Key;
+                    //if (fileName == "unity_builtin_extra") continue;
+
+                    var outputPath = Path.Combine(outputDirectory, $"{fileName}reference");
+
+                    am.PrepareNewBundle(path, out var bun, out var bundleAssetsFile, out var assetBundleExtAsset);
+                    // Update bundle assets name and bundle name to the name specified in outputAssetBundlePath
+                    var bundleBaseField = assetBundleExtAsset.instance.GetBaseField();
+                    bundleBaseField.SetValue("m_Name", fileName);
+                    bundleBaseField.SetValue("m_AssetBundleName", fileName);
+
+                    bundleAssetsFile.file.dependencies.dependencies.Clear();
+                    foreach (var dependency in resourcesInst.file.dependencies.dependencies)
+                        bundleAssetsFile.AddDependency(dependency.assetPath);
+
+                    UpdateAssetBundleDependencies(bundleBaseField, bundleAssetsFile.file.dependencies.dependencies);
+
+                    // Get container for populating asset listings
+                    var containerArray = bundleBaseField.GetField("m_Container/Array");
+                    foreach (var (asset, assetName, assetFileName, fileId, pathId, depth) in assets)
                     {
-                        assetBaseField.SetValue("m_StreamData/path", Path.Combine(dataDirectoryPath, assetBaseField.GetValue("m_StreamData/path").AsString()));
+                        var assetBaseField = asset.instance.GetBaseField();
+                        if (assetBaseField.GetFieldType() == "Texture2D")
+                        {
+                            string streamPath = assetBaseField.GetValue("m_StreamData/path").AsString();
+                            if (!string.IsNullOrEmpty(streamPath))
+                                assetBaseField.SetValue("m_StreamData/path", Path.Combine(dataDirectoryPath, streamPath));
+                        }
+
+                        var otherBytes = asset.instance.WriteToByteArray();
+                        var currentAssetReplacer = new AssetsReplacerFromMemory(0, pathId, (int)asset.info.curFileType,
+                                                                                AssetHelper.GetScriptIndex(asset.file.file, asset.info),
+                                                                                otherBytes);
+                        assetsReplacers.Add(currentAssetReplacer);
+
+                        // Create entry in m_Container to make this asset visible in the API, otherwise said the asset can be found with AssetBundles.LoadAsset* methods
+                        newContainerChildren.Add(containerArray.CreateEntry(assetName, 0, pathId));
                     }
 
-                    var otherBytes = asset.instance.WriteToByteArray();
-                    var currentAssetReplacer = new AssetsReplacerFromMemory(0, pathId, (int)asset.info.curFileType,
-                                                                            AssetHelper.GetScriptIndex(asset.file.file, asset.info),
-                                                                            otherBytes);
-                    assetsReplacers.Add(currentAssetReplacer);
+                    containerArray.SetChildrenList(newContainerChildren.ToArray());
+                    bundleBaseField.GetField("m_PreloadTable/Array").SetChildrenList(Array.Empty<AssetTypeValueField>());
 
-                    // Create entry in m_Container to make this asset visible in the API, otherwise said the asset can be found with AssetBundles.LoadAsset* methods
-                    newContainerChildren.Add(containerArray.CreateEntry(assetName, 0, pathId));
-                }
+                    //Save changes for building new bundle file
+                    var newAssetBundleBytes = bundleBaseField.WriteToByteArray();
+                    assetsReplacers.Add(new AssetsReplacerFromMemory(0, assetBundleExtAsset.info.index, (int)assetBundleExtAsset.info.curFileType, 0xFFFF, newAssetBundleBytes));
+                    assetsReplacers = assetsReplacers.OrderBy(repl => repl.GetPathID()).ToList();
 
-                containerArray.SetChildrenList(newContainerChildren.ToArray());
-                bundleBaseField.GetField("m_PreloadTable/Array").SetChildrenList(Array.Empty<AssetTypeValueField>());
+                    if (File.Exists(outputPath)) File.Delete(outputPath);
 
-                //Save changes for building new bundle file
-                var newAssetBundleBytes = bundleBaseField.WriteToByteArray();
-                assetsReplacers.Add(new AssetsReplacerFromMemory(0, assetBundleExtAsset.info.index, (int)assetBundleExtAsset.info.curFileType, 0xFFFF, newAssetBundleBytes));
-                assetsReplacers = assetsReplacers.OrderBy(repl => repl.GetPathID()).ToList();
-
-                if (File.Exists(outputAssetBundlePath)) File.Delete(outputAssetBundlePath);
-
-                byte[] newAssetData;
-                using (var bundleStream = new MemoryStream())
-                using (var writer = new AssetsFileWriter(bundleStream))
-                {
-                    //We need to order the replacers by their pathId for Unity to be able to read the Bundle correctly.
-                    bundleAssetsFile.file.Write(writer, 0, assetsReplacers, 0, am.classFile);
-                    newAssetData = bundleStream.ToArray();
-                }
-
-                using (var file = File.OpenWrite(outputAssetBundlePath))
-                using (var writer = new AssetsFileWriter(file))
-                {
-                    var assetsFileName = $"CAB-{GUID.Generate()}";
-                    bun.file.Write(writer, new List<BundleReplacer>
+                    byte[] newAssetData;
+                    using (var bundleStream = new MemoryStream())
+                    using (var writer = new AssetsFileWriter(bundleStream))
                     {
-                        new BundleReplacerFromMemory(bundleAssetsFile.name, "resources.assets", true, newAssetData, newAssetData.Length),
-                    });
+                        //We need to order the replacers by their pathId for Unity to be able to read the Bundle correctly.
+                        bundleAssetsFile.file.Write(writer, 0, assetsReplacers, 0, am.classFile);
+                        newAssetData = bundleStream.ToArray();
+                    }
+
+                    using (var file = File.OpenWrite(outputPath))
+                    using (var writer = new AssetsFileWriter(file))
+                    {
+                        var assetsFileName = $"CAB-{GUID.Generate()}";
+                        bun.file.Write(writer, new List<BundleReplacer>
+                        {
+                            new BundleReplacerFromMemory(bundleAssetsFile.name, fileName, true, newAssetData, newAssetData.Length),
+                        });
+                    }
                 }
 
                 pipeline.Log(LogLevel.Information, $"Finished Building Bundle", contexts.ToArray());
