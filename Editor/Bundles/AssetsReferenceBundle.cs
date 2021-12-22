cbb7ce2c4ff6145ccecfc76060822a36d535b34c
PassivePicasso <PassivePicasso@users.noreply.github.com>
2021-12-22 11:20:26 -0500

    Add support to assign internal custom shaders

--- Editor/Bundles/AssetsReferenceBundle.cs
+++ Editor/Bundles/AssetsReferenceBundle.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace BundleKit.Bundles
 {
@@ -15,47 +16,48 @@ namespace BundleKit.Bundles
         public AssetsReferenceBundle[] dependencies;
 
         AssetBundle bundle;
-        [InitializeOnLoadMethod]
-        static void ReloadAssetsReferenceBundles()
-        {
-            var arbs = AssetDatabase.FindAssets($"t:{nameof(AssetsReferenceBundle)}")
-                .Select(guid => AssetDatabase.GUIDToAssetPath(guid))
-                .Select(path => AssetDatabase.LoadAssetAtPath<AssetsReferenceBundle>(path))
-                .OrderBy(arb => arb.name)
-                .ToArray();
-            for (int i = 0; i < arbs.Length; i++)
-            {
-                var a = arbs[i];
-            }
-            arbs.First().Initialize();
-        }
 
-        private void Initialize(AssetBundle[] allBundles = null)
+        public void Initialize()
         {
             if (!EditorUtility.IsPersistent(this)) return;
             var path = AssetDatabase.GetAssetPath(this);
-            if (allBundles == null)
-                allBundles = AssetBundle.GetAllLoadedAssetBundles().ToArray();
-            bundle = allBundles
-                .FirstOrDefault(bnd => bnd.name.Contains(name));
+            var allBundles = AssetBundle.GetAllLoadedAssetBundles().ToArray();
+            bundle = allBundles.FirstOrDefault(bnd => bnd.name.Contains(name));
             if (!bundle)
                 bundle = AssetBundle.LoadFromFile(path);
+
+            Assets = bundle.LoadAllAssets();
             var shaders = bundle.LoadAllAssets<Shader>();
             foreach (var shader in shaders)
+            {
                 ShaderUtil.RegisterShader(shader);
+                if (name.Contains("globalgamemanagers"))
+                {
+                    switch (shader.name)
+                    {
+                        case "Hidden/Internal-DeferredReflections":
+                            GraphicsSettings.SetCustomShader(BuiltinShaderType.DeferredReflections, shader);
+                            break;
+                        case "Hidden/Internal-DeferredShading":
+                            GraphicsSettings.SetCustomShader(BuiltinShaderType.DeferredShading, shader);
+                            break;
+                    }
+                }
+            }
 
-            hideFlags = HideFlags.None;
-        }
-
-        private void Awake()
-        {
-            if (Assets == null) return;
             LoadedIds = new long[Assets.Length];
             for (int i = 0; i < Assets.Length; i++)
             {
                 var found = AssetDatabase.TryGetGUIDAndLocalFileIdentifier(Assets[i], out string guid, out long loadedId);
                 LoadedIds[i] = loadedId;
             }
+
+            hideFlags = HideFlags.None;
+        }
+
+        private void Awake()
+        {
+            Initialize();
         }
     }
 }
