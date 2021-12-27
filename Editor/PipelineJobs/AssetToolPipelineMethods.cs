cb79e7775d40b776620b941693b59e525664246f
PassivePicasso <PassivePicasso@users.noreply.github.com>
2021-12-27 16:17:01 -0500

    improve logging and progress reporting

--- Editor/PipelineJobs/AssetToolPipelineMethods.cs
+++ Editor/PipelineJobs/AssetToolPipelineMethods.cs
@@ -2,6 +2,7 @@
 using AssetsTools.NET.Extra;
 using BundleKit.Assets;
 using BundleKit.Utility;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -17,7 +18,6 @@ namespace BundleKit.PipelineJobs
             // Load assets files from Resources
             var assetsFileInst = am.LoadAssetsFile(assetsFilePath, false);
 
-            am.LoadClassDatabaseFromPackage(assetsFileInst.file.typeTree.unityVersion);
 
             return assetsFileInst;
         }
@@ -31,21 +31,18 @@ namespace BundleKit.PipelineJobs
             var assetBundleAsset = bundleAssetsFile.table.GetAssetsOfType((int)AssetClassID.AssetBundle)[0];
             assetBundleExtAsset = am.GetExtAsset(bundleAssetsFile, 0, assetBundleAsset.index);
         }
-        public static IEnumerable<AssetData> CollectAssets(AssetsManager am, AssetsFileInstance assetsFileInst, HashSet<AssetID> visited, Regex[] nameRegex, AssetClassID assetClass, ProgressBar progressBar)
+        public static IEnumerable<AssetData> CollectAssets(AssetsManager am, AssetsFileInstance assetsFileInst, HashSet<AssetID> visited, Regex[] nameRegex, AssetClassID assetClass, UpdateLog Update)
         {
             // Iterate over all requested Class types and collect the data required to copy over the required asset information
             // This step will recurse over dependencies so all required assets will become available from the resulting bundle
-            progressBar.Update(title: "Mapping PathIds to Resource Paths");
-            progressBar.Update(title: "Collecting Assets");
+            Update("Mapping PathIds to Resource Paths", log: false);
             var targetAssets = Enumerable.Empty<AssetData>();
             var fileInfos = assetsFileInst.table.GetAssetsOfType((int)assetClass);
-
             for (var x = 0; x < fileInfos.Count; x++)
             {
                 var assetFileInfo = fileInfos[x];
-                var name = AssetHelper.GetAssetNameFast(assetsFileInst.file, am.classFile, assetFileInfo);
-                progressBar.Update($"[Loading] ({(AssetClassID)assetFileInfo.curFileType}) {name}", $"Collecting Assets ({x} / {fileInfos.Count})", progress: x / (float)fileInfos.Count);
 
+                var name = AssetHelper.GetAssetNameFast(assetsFileInst.file, am.classFile, assetFileInfo);
                 // If a name Regex filter is applied, and it does not match, continue
                 int i = 0;
                 for (; i < nameRegex.Length; i++)
@@ -61,8 +58,11 @@ namespace BundleKit.PipelineJobs
                 if (ext.file.name.Contains("unity_builtin_extra"))
                     continue;
 
+                name = ext.GetName(am);
+                Update($"Scanning Assets ({x} / {fileInfos.Count})", $"[Loading] ({(AssetClassID)assetFileInfo.curFileType}) {name}", x / (float)fileInfos.Count, false);
+
                 // Find name, path and fileId of each asset referenced directly and indirectly by assetFileInfo including itself
-                targetAssets = targetAssets.Concat(assetsFileInst.GetDependentAssetIds(visited, ext.instance.GetBaseField(), am, progressBar, true)
+                targetAssets = targetAssets.Concat(assetsFileInst.GetDependentAssetIds(visited, ext.instance.GetBaseField(), am, Update, true)
                                            .Prepend((ext, name, assetsFileInst.name, 0, assetFileInfo.index, 0)));
             }
 
@@ -76,7 +76,7 @@ namespace BundleKit.PipelineJobs
         public static void AddDependency(this AssetsFileInstance assetsFileInst, AssetsFileDependency assetsFileDependency)
         {
             var dependencies = assetsFileInst.file.dependencies.dependencies;
-            
+
             var assetsFilePath = assetsFileDependency.assetPath;
             var fixedPath = assetsFilePath;
             if (assetsFilePath != unityBuiltinExtra && assetsFilePath != unityDefaultResources)
